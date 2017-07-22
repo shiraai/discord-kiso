defd113dc2104f03a33aad1cb5430929e698d9f1
rekyuu <senkan.re.kyuu@gmail.com>
2017-07-22 15:24:40 -0600

    lol here we go

--- lib/discord_kiso/bot.ex
+++ lib/discord_kiso/bot.ex
@@ -537,6 +537,7 @@ defmodule DiscordKiso.Bot do
   def del_custom_command(msg) do
     [_ | [command | _]] = msg.content |> String.split
     action = query_data(:commands, "#{guild_id}_!#{command}")
+    guild_id = Nostrum.Api.get_channel!(msg.channel_id)["guild_id"]
 
     case action do
       nil -> reply "Command does not exist."
