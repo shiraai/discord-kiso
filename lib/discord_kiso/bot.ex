2d1f2faea6dc7eeb629f23ec18fa3bdb97350883
rekyuu <senkan.re.kyuu@gmail.com>
2017-07-22 23:27:59 -0600

    role checks

--- lib/discord_kiso/bot.ex
+++ lib/discord_kiso/bot.ex
@@ -74,6 +74,10 @@ defmodule DiscordKiso.Bot do
       match "!stoplog", :del_log_channel
       match "!addhere", :add_log_user
       match "!delhere", :del_log_user
+      match "!setmention", :set_mention_role
+      match "!stopmention", :del_mention_role
+      match "!streamrole", :set_stream_announce
+      match "!streamany", :del_stream_announce
       match "!add", :add_custom_command
       match "!del", :del_custom_command
     end
@@ -84,69 +88,85 @@ defmodule DiscordKiso.Bot do
     user_id = msg.user.id
     {:ok, member} = Nostrum.Api.get_member(guild_id, user_id)
     username = member["user"]["username"]
+    db = query_data("guilds", guild_id)
+
+    announce? = case db.stream_role do
+      nil ->
+      stream_role -> Enum.member?(for role <- member["roles"] do
+        {role_id, _} = role |> Integer.parse
+        stream_role == role_id
+      end, true)
+    end
 
     if msg.game do
       if msg.game.type do
         case msg.game.type do
           0 -> remove_streamer(guild_id, user_id)
           1 ->
-            stream_title = msg.game.name
-            stream_url = msg.game.url
-            twitch_username = msg.game.url |> String.split("/") |> List.last
-            db = query_data("guilds", guild_id)
-            log_chan = db.log
+            if announce? do
+              stream_title = msg.game.name
+              stream_url = msg.game.url
+              twitch_username = msg.game.url |> String.split("/") |> List.last
+              log_chan = db.log
 
-            stream_list = query_data("streams", guild_id)
+              stream_list = query_data("streams", guild_id)
 
-            stream_list = case stream_list do
-              nil -> []
-              streams -> streams
-            end
-
-            unless Enum.member?(stream_list, user_id) do
-              store_data("streams", guild_id, stream_list ++ [user_id])
-              alert_roles = db.mention_roles
-              alert_users = db.mention_users
-
-              is_alert? = cond do
-                Enum.member?(alert_users, user_id) -> true
-                true -> Enum.member?(for role <- member["roles"] do
-                  {role_id, _} = role |> Integer.parse
-                  Enum.member?(alert_roles, role_id)
-                end, true)
+              stream_list = case stream_list do
+                nil -> []
+                streams -> streams
               end
 
-              here = cond do
-                is_alert? -> " @here"
-                true -> ""
+              unless Enum.member?(stream_list, user_id) do
+                store_data("streams", guild_id, stream_list ++ [user_id])
+                alert_roles = db.mention_roles
+                alert_users = db.mention_users
+                role_mention = db.mention
+
+                is_alert? = cond do
+                  Enum.member?(alert_users, user_id) -> true
+                  true -> Enum.member?(for role <- member["roles"] do
+                    {role_id, _} = role |> Integer.parse
+                    Enum.member?(alert_roles, role_id)
+                  end, true)
+                end
+
+                mention = case role_mention do
+                  nil -> "@here"
+                  role -> "<@&#{role}>"
+                end
+
+                here = cond do
+                  is_alert? -> " #{mention}"
+                  true -> ""
+                end
+
+                message = "**#{username}** is now live on Twitch!#{here}"
+
+                twitch_user = "https://api.twitch.tv/kraken/users?login=#{twitch_username}"
+                headers = %{"Accept" => "application/vnd.twitchtv.v5+json", "Client-ID" => "#{Application.get_env(:discord_kiso, :twitch_client_id)}"}
+
+                request = HTTPoison.get!(twitch_user, headers)
+                response = Poison.Parser.parse!((request.body), keys: :atoms)
+                user = response.users |> List.first
+
+                user_channel = "https://api.twitch.tv/kraken/channels/#{user._id}"
+                user_info_request = HTTPoison.get!(user_channel, headers)
+                user_info_response = Poison.Parser.parse!((user_info_request.body), keys: :atoms)
+
+                game = case user_info_response.game do
+                  nil -> "streaming on Twitch.tv"
+                  game -> "playing #{game}"
+                end
+
+                reply [content: message, embed: %Nostrum.Struct.Embed{
+                  color: 0x4b367c,
+                  title: "#{twitch_username} #{game}",
+                  url: "#{stream_url}",
+                  description: "#{stream_title}",
+                  thumbnail: %Nostrum.Struct.Embed.Thumbnail{url: "#{user.logo}"},
+                  timestamp: "#{DateTime.utc_now() |> DateTime.to_iso8601()}"
+                }], chan: log_chan
               end
-
-              message = "**#{username}** is now live on Twitch!#{here}"
-
-              twitch_user = "https://api.twitch.tv/kraken/users?login=#{twitch_username}"
-              headers = %{"Accept" => "application/vnd.twitchtv.v5+json", "Client-ID" => "#{Application.get_env(:discord_kiso, :twitch_client_id)}"}
-
-              request = HTTPoison.get!(twitch_user, headers)
-              response = Poison.Parser.parse!((request.body), keys: :atoms)
-              user = response.users |> List.first
-
-              user_channel = "https://api.twitch.tv/kraken/channels/#{user._id}"
-              user_info_request = HTTPoison.get!(user_channel, headers)
-              user_info_response = Poison.Parser.parse!((user_info_request.body), keys: :atoms)
-
-              game = case user_info_response.game do
-                nil -> "streaming on Twitch.tv"
-                game -> "playing #{game}"
-              end
-
-              reply [content: message, embed: %Nostrum.Struct.Embed{
-                color: 0x4b367c,
-                title: "#{twitch_username} #{game}",
-                url: "#{stream_url}",
-                description: "#{stream_title}",
-                thumbnail: %Nostrum.Struct.Embed.Thumbnail{url: "#{user.logo}"},
-                timestamp: "#{DateTime.utc_now() |> DateTime.to_iso8601()}"
-              }], chan: log_chan
             end
         end
       end
@@ -468,7 +488,7 @@ defmodule DiscordKiso.Bot do
 
     cond do
       db == nil ->
-        store_data("guilds", guild_id, %{admin_roles: [], log: nil, mention_roles: [], mention_users: []})
+        store_data("guilds", guild_id, %{admin_roles: [], log: nil, mention_roles: [], mention_users: [], mention: nil, stream_role: nil})
         reply "Hey. Be sure to add an admin role to manage my settings using `!addrole <role>`."
       db.admin_roles == [] -> reply "No admin roles set, anyone can edit my settings! Change this with `!addrole <role>`."
       true -> reply "I'm ready to sortie!"
@@ -530,6 +550,44 @@ defmodule DiscordKiso.Bot do
     db = Map.put(db, :log, nil)
     store_data("guilds", guild_id, db)
     reply "Okay, I will no longer announce streams."
+  end
+
+  def set_mention_role(msg) do
+    guild_id = Nostrum.Api.get_channel!(msg.channel_id)["guild_id"]
+    db = query_data("guilds", guild_id)
+    role = msg.mention_roles |> List.first
+
+    db = Map.put(db, :mention, role)
+    store_data("guilds", guild_id, db)
+    reply "Okay, I will alert members of that role."
+  end
+
+  def del_mention_role(msg) do
+    guild_id = Nostrum.Api.get_channel!(msg.channel_id)["guild_id"]
+    db = query_data("guilds", guild_id)
+
+    db = Map.put(db, :mention, nil)
+    store_data("guilds", guild_id, db)
+    reply "Okay, any alerts will only be for online users."
+  end
+
+  def set_stream_announce(msg) do
+    guild_id = Nostrum.Api.get_channel!(msg.channel_id)["guild_id"]
+    db = query_data("guilds", guild_id)
+    role = msg.mention_roles |> List.first
+
+    db = Map.put(db, :stream_role, role)
+    store_data("guilds", guild_id, db)
+    reply "Okay, I will only announce streams for members of that role."
+  end
+
+  def del_stream_announce(msg) do
+    guild_id = Nostrum.Api.get_channel!(msg.channel_id)["guild_id"]
+    db = query_data("guilds", guild_id)
+
+    db = Map.put(db, :stream_role, nil)
+    store_data("guilds", guild_id, db)
+    reply "Okay, anyone on this server will be announced when they go live."
   end
 
   def add_log_user(msg) do
