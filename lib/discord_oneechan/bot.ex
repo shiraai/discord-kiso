2e51c995d7a6ec0c84cd1d867fb837bcab5e5ea2
rekyuu <senkan.re.kyuu@gmail.com>
2017-07-28 23:46:22 -0600

    admin enforcement

--- lib/discord_oneechan/bot.ex
+++ lib/discord_oneechan/bot.ex
@@ -2,16 +2,26 @@ defmodule DiscordOneechan.Bot do
   use DiscordOneechan.Module
   import DiscordOneechan.Util
 
+  # Enforcers
+  def admin(msg) do
+    user_id = 66654117510000640
+    msg.author.id == user_id
+  end
+
   # Event handlers
   handle :MESSAGE_CREATE do
     match "!help", do: reply "https://github.com/shiraai/discord-oneechan"
-    match "!ping", :ping
+
+    enforce :admin do
+      match "!ping", :ping
+    end
   end
 
   def handle_event(_, state) do
     {:ok, state}
   end
 
+  # Administrative commands
   def ping(msg) do
     IO.inspect msg
     reply "Pong!"
