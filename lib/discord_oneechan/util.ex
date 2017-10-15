a1974abf410ae8f7e000e8c5ae24b7f0211a8b4c
rekyuu <senkan.re.kyuu@gmail.com>
2017-10-14 20:29:34 -0600

    removed update_db

--- lib/discord_oneechan/util.ex
+++ lib/discord_oneechan/util.ex
@@ -48,24 +48,4 @@ defmodule DiscordOneechan.Util do
     :dets.close(table)
     response
   end
-
-  def update_db do
-    # Updates roles
-    for {message_id, role_id} <- query_all_data(:roles) do
-      delete_data :roles, message_id
-      store_data :roles, Integer.to_string(message_id), Integer.to_string(role_id)
-    end
-
-    # Updates chans
-    delete_data :chans, 0
-    store_data :chans, 0, ["340760692934180864"]
-
-    # Updates commands
-    delete_data :commands, :roles
-    for {command, role_id} <- query_all_data(:roles) do
-      delete_data :commands, command
-      store_data :commands, command, Integer.to_string(role_id)
-    end
-    store_data :chans, :roles, ["305341291158437888", "305341344031703042", "305341347106127872","305341400114003989", "315608541425565699", "315608547218030593", "315608550418284555", "315613276689924098", "315613315017736194", "305352524133564418", "305352524133564418"]
-  end
 end
